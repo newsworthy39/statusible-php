f4f0b5a706193da28b6ef0769374facb18c960f2
newsworthy39 <newsworthy39@github.com>
2019-09-05 16:05:37 +0200

    Initial PSR-7 compatible Hello-world

--- index.php
+++ index.php
@@ -2,37 +2,33 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-use \newsworthy39\Backend;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 
-if(false != strstr($_SERVER['HTTP_USER_AGENT'],"GitHub-Hookshot")) {
+$request = Zend\Diactoros\ServerRequestFactory::fromGlobals(
+    $_SERVER, $_GET, $_POST, $_COOKIE, $_FILES
+);
 
-        // get container
-        $app = new Config();
+$router = new League\Route\Router;
 
-        // Parameters passed using a named array:
-        $conn = $app->redis();
+// map a route
+$router->map('GET', '/queue/{id}', function (ServerRequestInterface $request) : ResponseInterface {
+    $response = new Zend\Diactoros\Response;
+    $response->getBody()->write('<h1>Hello, World!</h1>');
+    return $response;
+});
 
-        $client = new Predis\Client($conn);
+// map a route
+$router->map('GET', '/', function (ServerRequestInterface $request) : ResponseInterface {
+        $response = new Zend\Diactoros\Response;
+        $response->getBody()->write('<h1>Hello, World!</h1>');
+        return $response;
+    });
 
-        $backend = new Backend();
+$response = $router->dispatch($request);
 
-        // when dealing with queues and hook-shots, we fetch our secret based on the queue-id:
-        list($omit, $omit2, $queue_id) = explode('/',$_SERVER['REQUEST_URI']);
-        $secret = $backend->secrets($queue_id);
-
-        if ($secret != false) {
-                $post_data = file_get_contents('php://input');
-                $signature = sprintf("sha1=%s", hash_hmac('sha1', $post_data, $secret));
-
-                // We have verified, that it is in fact a valid request.
-                if ( $_SERVER['HTTP_X_HUB_SIGNATURE'] == $signature ) {
-                    $post_data= urldecode($post_data);
-                    list($key, $payload) = explode('=', $post_data, 2);
-                    $client->publish('workqueue-github', $payload);
-                    echo "Signature-verified Github Hook-shot.";
-                }
-        }
-}
+// send the response to the browser
+(new Zend\HttpHandlerRunner\Emitter\SapiEmitter)->emit($response);
 
 ?>
 
---
+++ src/GithubHookshot.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types = 1);
+
+require __DIR__ . '/vendor/autoload.php';
+
+use \newsworthy39\Backend;
+
+if(false != strstr($_SERVER['HTTP_USER_AGENT'],"GitHub-Hookshot")) {
+
+// get container
+$app = new Config();
+
+// Parameters passed using a named array:
+$conn = $app->redis();
+
+$client = new Predis\Client($conn);
+
+$backend = new Backend();
+
+// when dealing with queues and hook-shots, we fetch our secret based on the queue-id:
+list($omit, $omit2, $queue_id) = explode('/',$_SERVER['REQUEST_URI']);
+$secret = $backend->secrets($queue_id);
+
+if ($secret != false) {
+        $post_data = file_get_contents('php://input');
+        $signature = sprintf("sha1=%s", hash_hmac('sha1', $post_data, $secret));
+
+        // We have verified, that it is in fact a valid request.
+        if ( $_SERVER['HTTP_X_HUB_SIGNATURE'] == $signature ) {
+            $post_data= urldecode($post_data);
+            list($key, $payload) = explode('=', $post_data, 2);
+            $client->publish('workqueue-github', $payload);
+            echo "Signature-verified Github Hook-shot.";
+        }
+}
+}
