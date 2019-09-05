db6be7aa4e8c6cab2771ef486fb26e441f4d3b98
newsworthy39 <newsworthy39@github.com>
2019-09-05 20:51:46 +0200

    Added bootstrap and front example

--- index.php
+++ index.php
@@ -4,18 +4,18 @@ require __DIR__ . '/vendor/autoload.php';
 
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
-use League\Route\Router;
-use League\Plates\Engine;
 
 $request = Zend\Diactoros\ServerRequestFactory::fromGlobals(
     $_SERVER, $_GET, $_POST, $_COOKIE, $_FILES
 );
 
+// Add elements to container
 $container = (new League\Container\Container)->defaultToShared();
 $container->add(League\Plates\Engine::class)->addArgument('templates/');
 $container->add(League\Route\Router::class);
 $container->add(newsworthy39\Config::class);
 
+// Wire different things together.
 $router = $container->get(League\Route\Router::class);
 $templates = $container->get(League\Plates\Engine::class);
 $templates->registerFunction('variables', function($string) use ($container) {
@@ -23,23 +23,23 @@ $templates->registerFunction('variables', function($string) use ($container) {
     return $config->variables($string);
 });
 
-// map a route
+// Map some routes, but use json output.
 $router->map('GET', '/queue/{id}', function (ServerRequestInterface $request) : ResponseInterface {
     $response = new Zend\Diactoros\Response;
     $response->getBody()->write('<h1>Hello, api!</h1>');
     return $response;
 });
 
-// map a route
 $router->map('GET', '/', function (ServerRequestInterface $request) use ($container) : ResponseInterface {
 
         // Render a template
         $templates = $container->get(League\Plates\Engine::class);
         $response = new Zend\Diactoros\Response;
-        $response->getBody()->write($templates->render('front', ['name' => $_SERVER['HTTP_X_FORWARDED_FOR']]));
+        $response->getBody()->write($templates->render('front'));
         return $response;
     });
 
+// And dispatch the request.
 $response = $router->dispatch($request);
 
 // send the response to the browser
--- templates/front.php
+++ templates/front.php
@@ -1,4 +1,114 @@
-<?php $this->layout('template', ['title' => 'welcome']) ?>
+<?php $this->layout('template', ['title' => 'sign-in']) ?>
 
-<h1><?=$this->variables('site_title')?> </h1>
-<p>Hello, <?=$this->e($name)?></p>
+<div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-primary border-bottom shadow-sm">
+  <h5 class="my-0 mr-md-auto font-weight-normal"><?=$this->variables('site_title')?></h5>
+  <nav class="my-2 my-md-0 mr-md-3">
+    <a class="p-2 text-dark" href="#">Features</a>
+    <a class="p-2 text-dark" href="#">Enterprise</a>
+    <a class="p-2 text-dark" href="#">Support</a>
+    <a class="p-2 text-dark" href="#">Pricing</a>
+  </nav>
+  <a class="btn btn-outline-primary" href="#">Sign up</a>
+</div>
+
+<div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+  <h1 class="display-4">Pricing</h1>
+  <p class="lead">Affordable and easy pricing, lets you proactively monitor service status, and engage customers on incidents</p>
+</div>
+
+<div class="container">
+  <div class="card-deck mb-3 text-center">
+    <div class="card mb-4 shadow-sm">
+      <div class="card-header">
+        <h4 class="my-0 font-weight-normal">Free for personal use</h4>
+      </div>
+      <div class="card-body">
+        <h1 class="card-title pricing-card-title">$0 <small class="text-muted">/ mo</small></h1>
+        <ul class="list-unstyled mt-3 mb-4">
+          <li>1 user</li>
+          <li>512 mb of storage</li>
+          <li>Service-checks included</li>
+          <li>
+          <li>Email support</li>
+          <li>Help center access</li>
+        </ul>
+        <button type="button" class="btn btn-lg btn-block btn-outline-primary">Sign up for free</button>
+      </div>
+    </div>
+    <div class="card mb-4 shadow-sm">
+      <div class="card-header">
+        <h4 class="my-0 font-weight-normal">Professionel</h4>
+      </div>
+      <div class="card-body">
+        <h1 class="card-title pricing-card-title">$15 <small class="text-muted">/ mo</small></h1>
+        <ul class="list-unstyled mt-3 mb-4">
+          <li>Team support</li>
+          <li>20 users included</li>
+          <li>20 checks included</li>
+          <li>5 Service-pages included</li>
+          <li>5 GB of storage</li>
+          <li>Company branding options</li>
+          <li>Priority email support</li>
+          <li>Help center access</li>
+        </ul>
+        <button type="button" class="btn btn-lg btn-block btn-primary">Get started</button>
+      </div>
+    </div>
+    <div class="card mb-4 shadow-sm">
+      <div class="card-header">
+        <h4 class="my-0 font-weight-normal">Enterprise</h4>
+      </div>
+      <div class="card-body">
+        <h1 class="card-title pricing-card-title">$29 <small class="text-muted">/ mo</small></h1>
+        <ul class="list-unstyled mt-3 mb-4">
+          <li>Team support</li>
+          <li>Service-pages included</li>
+          <li>Service-checks included</li>
+          <li>15 GB of storage</li>
+          <li>Company branding options</li>
+          <li>Phone and email support</li>
+          <li>Help center access</li>
+        </ul>
+        <button type="button" class="btn btn-lg btn-block btn-primary">Contact us</button>
+      </div>
+    </div>
+  </div>
+
+  <footer class="pt-4 my-md-5 pt-md-5 border-top">
+    <div class="row">
+      <div class="col-12 col-md">
+        <img class="mb-2" src="/docs/4.3/assets/brand/bootstrap-solid.svg" alt="" width="24" height="24">
+        <small class="d-block mb-3 text-muted">&copy; 2017-2019</small>
+      </div>
+      <div class="col-6 col-md">
+        <h5>Features</h5>
+        <ul class="list-unstyled text-small">
+          <li><a class="text-muted" href="#">Cool stuff</a></li>
+          <li><a class="text-muted" href="#">Random feature</a></li>
+          <li><a class="text-muted" href="#">Team feature</a></li>
+          <li><a class="text-muted" href="#">Stuff for developers</a></li>
+          <li><a class="text-muted" href="#">Another one</a></li>
+          <li><a class="text-muted" href="#">Last time</a></li>
+        </ul>
+      </div>
+      <div class="col-6 col-md">
+        <h5>Resources</h5>
+        <ul class="list-unstyled text-small">
+          <li><a class="text-muted" href="#">Resource</a></li>
+          <li><a class="text-muted" href="#">Resource name</a></li>
+          <li><a class="text-muted" href="#">Another resource</a></li>
+          <li><a class="text-muted" href="#">Final resource</a></li>
+        </ul>
+      </div>
+      <div class="col-6 col-md">
+        <h5>About</h5>
+        <ul class="list-unstyled text-small">
+          <li><a class="text-muted" href="#">Team</a></li>
+          <li><a class="text-muted" href="#">Locations</a></li>
+          <li><a class="text-muted" href="#">Privacy</a></li>
+          <li><a class="text-muted" href="#">Terms</a></li>
+        </ul>
+      </div>
+    </div>
+  </footer>
+</div>
