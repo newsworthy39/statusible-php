3c2fcdc2267e0e6c988884aaaec6b295caf2ef3a
newsworthy39 <newsworthy39@github.com>
2019-09-19 11:22:05 +0200

    Malformed insert into migrations-tables

--- src/Factory/Tinker.php
+++ src/Factory/Tinker.php
@@ -26,13 +26,13 @@ class Tinker
                 $statements = $object->up();
                 foreach ($statements as $statement) {
                     $status = $this->pdo->exec($statement);
-
-                    // insert into migrations.
-                    $sql = "INSERT INTO `migrations` (classname) VALUES (:classname)";
-                    $statement = $this->pdo->prepare($sql);
-                    $statement->execute(array('classname' => $class));
-                    $statement = null;
                 }
+
+                // insert into migrations.
+                $sql = "INSERT INTO `migrations` (classname) VALUES (:classname)";
+                $statement = $this->pdo->prepare($sql);
+                $statement->execute(array('classname' => $class));
+                $statement = null;
             }
         } catch (PDOException $e) {
             printf("Error %s\n", $e->getMessage()); //Remove or change message in production code
@@ -48,11 +48,11 @@ class Tinker
                 foreach ($statements as $statement) {
                     $this->pdo->exec($statement);
 
-                      // remove from  migrations.
-                      $sql = "DELETE FROM `migrations` WHERE classname = :classname";
-                      $statement = $this->pdo->prepare($sql);
-                      $statement->execute(array('classname' => $class));
-                      $statement = null;
+                    // remove from  migrations.
+                    $sql = "DELETE FROM `migrations` WHERE classname = :classname";
+                    $statement = $this->pdo->prepare($sql);
+                    $statement->execute(array('classname' => $class));
+                    $statement = null;
                 }
             }
         } catch (PDOException $e) {
