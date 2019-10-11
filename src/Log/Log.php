0a82e1757a6b33b4f1f0343cf4fce350d606065e
newsworthy39 <newsworthy39@github.com>
2019-10-11 13:12:41 +0200

    added syslog

--- src/Log/Log.php
+++ src/Log/Log.php
@@ -7,52 +7,52 @@ namespace newsworthy39\Log;
 
 class Log
 {
-
-    public const LOG_INFO = 0;
-    public const LOG_WARNING = 1;
-    public const LOG_ERROR = 2;
-    public const LOG_DEBUG = 3;
-
     private $logLevel = LOG_INFO;
 
     public function __construct()
     {
         $tmp = getenv('LOG_LEVEL');
         if (is_string($tmp)) {
-                if (strtolower($tmp) == "log_debug") {
-                    $this->logLevel = LOG_DEBUG;
-                }
-                if (strtolower($tmp) == "log_info") {
-                    $this->logLevel = LOG_INFO;
-                }
-                if (strtolower($tmp) == "log_error") {
-                    $this->logLevel = LOG_ERROR;
-                }
-                if (strtolower($tmp) == "log_warning") {
-                    $this->logLevel = LOG_WARNING;
-                }
+            if (strtolower($tmp) == "log_debug") {
+                $this->logLevel = LOG_DEBUG;
+            }
+            if (strtolower($tmp) == "log_info") {
+                $this->logLevel = LOG_INFO;
+            }
+            if (strtolower($tmp) == "log_error") {
+                $this->logLevel = LOG_ERROR;
+            }
+            if (strtolower($tmp) == "log_warning") {
+                $this->logLevel = LOG_WARNING;
+            }
         }
-        printf("%s: Logger starting loglevel %d \n", date("r"), $this->logLevel);
+        openlog("app", LOG_PID | LOG_PERROR, LOG_SYSLOG);
+        syslog($this->logLevel, sprintf("%s: Logger starting loglevel %d \n", date("r"), $this->logLevel));
+    }
+
+    public function __destruct()
+    {
+        closelog();
     }
 
     public function info(String $format)
     {
         if ($this->logLevel >= LOG_INFO) {
-            vprintf(sprintf("%s: %s", date("r"), $format), array_slice(func_get_args(),1));
+            syslog($this->logLevel, vsprintf(sprintf("%s: %s", date("r"), $format), array_slice(func_get_args(), 1)));
         }
     }
 
     public function error(String $format)
     {
         if ($this->logLevel >= LOG_ERROR) {
-            vprintf(sprintf("%s: %s", date("r"), $format), array_slice(func_get_args(),1));
+            syslog($this->logLevel,  vsprintf(sprintf("%s: %s", date("r"), $format), array_slice(func_get_args(), 1)));
         }
     }
 
     public function debug(String $format)
     {
         if ($this->logLevel >= LOG_DEBUG) {
-            vprintf(sprintf("%s: %s", date("r"), $format), array_slice(func_get_args(),1));
+            syslog($this->logLevel, vsprintf(sprintf("%s: %s", date("r"), $format), array_slice(func_get_args(), 1)));
         }
     }
 }
